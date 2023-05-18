fb82347c12ea2e8f6f055f69b617d051c842815d
norseguyuk81 <133810375+norseguyuk81@users.noreply.github.com>
2023-05-18 14:40:27 +0100

    Update config.ts

--- scripts/data/config.ts
+++ scripts/data/config.ts
@@ -282,7 +282,7 @@ export default {
          * Change your password frequently and only share with trusted sources.
          */
         encryption: {
-            password: "JODA82",
+            password: "XASpLXi-rEZObocred21",
         },
     },
 };
